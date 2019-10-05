f9b32cd574147ee92cdc372ef5b550e7fed36dbb
BuildTools <unconfigured@null.spigotmc.org>
2019-10-05 16:19:37 +0100

    Small test changes

---
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public Transform follow;
+
+    private float maxDistance = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Want to run after player movement
+    void LateUpdate()
+    {
+        
+    }
+}
