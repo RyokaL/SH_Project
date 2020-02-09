38955adc510d4a8e44c2fdcc8137738a0528698b
RyokaL <35602890+RyokaL@users.noreply.github.com>
2020-02-09 15:15:43 +0000

    Fixed terrain gen and used it to generate 'caves'

--- Assets/Scripts/Generation/HeightMapGen.cs
+++ Assets/Scripts/Generation/HeightMapGen.cs
@@ -6,6 +6,10 @@ public class HeightMapGen : MonoBehaviour
 {
     private MeshFilter thisMesh;
     private MeshCollider mCollide;
+
+    public float maxHeight = 2;
+    public float minHeight = 0.5f;
+
     void Awake()
     {
         thisMesh = GetComponent<MeshFilter>();
@@ -13,11 +17,16 @@ public class HeightMapGen : MonoBehaviour
     }
 
     public void generateHeightMap() {
+        float seed = Random.value * 100;
+        float height = Random.Range(minHeight, maxHeight);
         Vector3[] vertices = thisMesh.mesh.vertices;
         int verticesLength = (int)Mathf.Sqrt(vertices.Length);
         for(int i = 0; i < verticesLength; i++) {
             for(int j = 0; j < verticesLength; j++) {
-                vertices[(i * j) + j].y = Mathf.Abs(Mathf.PerlinNoise(i/ 10, j / 10) * 1.5f);
+                if(i == 0 || j == 0 || i == verticesLength - 1 || j == verticesLength - 1) {
+                    continue;
+                }
+                vertices[(i * (verticesLength)) + j].y = Mathf.PerlinNoise(seed + (float)i, seed + (float)j) * height;
             }
         }
         thisMesh.mesh.vertices = vertices;
