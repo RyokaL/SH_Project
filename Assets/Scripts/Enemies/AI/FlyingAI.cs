aaa639cc1835c88ae7f483b5ed492c3e92077f93
RyokaL <35602890+RyokaL@users.noreply.github.com>
2020-02-26 16:00:58 +0000

    Added Flying enemies

---
+++ Assets/Scripts/Enemies/AI/FlyingAI.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Enemy AI", menuName = "Enemy AI/Flying")]
+public class FlyingAI : AI {
+
+    private float cooldown = 0;
+
+    private float attackCooldown = 0;
+    private float ATK_COOL = 2;
+    private int atkCount = 0;
+    private int MAX_ATK = 3;
+    
+    public override void nextUpdate(GameObject avatar, EnemyStats stats) {
+        Transform avTransform = avatar.transform;
+        Collider[] playerInRange = Physics.OverlapSphere(avTransform.position, stats.sightRange);
+        foreach(Collider c in playerInRange) {
+            if(c.gameObject.tag == "Player") {
+                if(Vector3.Angle(avTransform.position, c.transform.position) < stats.sightAngle) {
+                    //Cast a ray to see if player is visible
+                    RaycastHit hit;
+                    //We want to ignore other enemy colliders
+                    LayerMask mask = ~(1 << 10);
+                    Physics.Raycast(avTransform.position, (c.transform.position - avTransform.position), out hit, stats.sightRange, mask);
+                    Debug.DrawRay(avTransform.position, (c.transform.position - avTransform.position));
+                    //If the collider we hit is the player, we have a direct line
+                    if(hit.collider.tag == "Player") {
+                        avTransform.LookAt(c.transform);
+                        Vector3 dir = (c.transform.position - avTransform.position).normalized;
+                        avTransform.position = avTransform.position + (dir * stats.speed * Time.fixedDeltaTime);
+                    }
+
+                    if(attackCooldown >= ATK_COOL) {
+                        cooldown += Time.deltaTime;
+                        if(cooldown >= (1 / stats.modifiers.fireRate) ) {
+                            stats.attack.fire(stats.modifiers, avTransform, c.transform);
+                            atkCount += 1;
+                            cooldown -= (1 / stats.modifiers.fireRate);
+                        }
+                        if(atkCount >= MAX_ATK) {
+                            atkCount = 0;
+                            attackCooldown -= ATK_COOL;
+                        }
+                    }
+                    else {
+                        attackCooldown += Time.deltaTime;
+                    }
+                    break;
+                } 
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Spells/SpellFire/FlyingFastSpell.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Spells", menuName = "Spells/FlyingShadow")]
+public class FlyingFastSpell : Spell
+{
+
+    public override void fire(SpellMod modifiers, Transform firePoint, Transform target) {
+        GameObject temp = Instantiate(projectile) as GameObject;
+        temp.transform.position = firePoint.position;
+
+        ISpellCollision spellScript = temp.GetComponent<ISpellCollision>();
+        spellScript.setModifiers(modifiers);
+
+        Rigidbody projectRigid = temp.GetComponent<Rigidbody>();
+        projectRigid.velocity = (target.position - firePoint.position).normalized * 25;
+    }
+
+    public override void fire(SpellMod modifiers, Transform firePoint, Camera mainCam) {
+        return;
+    }
+
+    public override GameObject getProjectile() {
+        return projectile;
+    }
+    public override SpellAttr GetSpellAttr() {
+        return spellAttributes;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
