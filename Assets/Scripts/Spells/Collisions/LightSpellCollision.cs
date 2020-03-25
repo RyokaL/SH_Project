d1f5ee4ee187bc3f4c5a159999f4f9b90a564570
RyokaL <35602890+RyokaL@users.noreply.github.com>
2020-03-25 16:28:08 +0000

    Implemented HitScan weapon

---
+++ Assets/Scripts/Spells/Collisions/LightSpellCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSpellCollision : MonoBehaviour, ISpellCollision
+{
+    private SpellMod modifiers;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.layer == 0) {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    // void Update() {
+    //     timer += Time.deltaTime;
+    //     Debug.Log(timer);
+    //     if(timer >= modifiers.range) {
+    //         GetComponent<Rigidbody>().velocity = Vector3.zero;
+    //     }
+    // }
+
+    public void setModifiers(SpellMod modifiers) {
+        this.modifiers = modifiers;
+        Destroy(gameObject, modifiers.TTL);
+    }
+}
---
+++ Assets/Scripts/Spells/SpellFire/LightSpell.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Spells", menuName = "Spells/Light")]
+public class LightSpell : Spell
+{
+    public string spellName;
+
+    private bool fired = false;
+    private Vector3 newPos;
+    private GameObject currProjectile = null;
+
+    public override void fire(SpellMod modifiers, Transform firePoint, Camera mainCam) {
+        GameObject temp = Instantiate(projectile, mainCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, mainCam.nearClipPlane)) - mainCam.transform.forward.normalized * 5, firePoint.rotation);
+        RaycastHit[] hits;
+
+        ISpellCollision spellCollision = temp.GetComponent<ISpellCollision>();
+
+        temp.GetComponent<Rigidbody>().velocity = mainCam.transform.forward.normalized * 250;
+        hits = Physics.RaycastAll(mainCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, mainCam.nearClipPlane)), mainCam.transform.forward);
+        if(hits.Length > 0) {
+            Array.Reverse(hits);
+            foreach(RaycastHit hit in hits) {
+                if(hit.collider.tag == "Enemy") {
+                    HealthControl collidedHealth = hit.collider.GetComponent<HealthControl>();
+                    collidedHealth.takeDamage(modifiers.damage * modifiers.damagePercent);
+
+                    if(modifiers.dot) {
+                        collidedHealth.applyDot(modifiers.dotTick, modifiers.dotLength);
+                    }
+                }
+                else if(hit.collider.tag == "EnemyAttack") {
+                    Destroy(hit.collider.gameObject);
+                }
+                else if(hit.collider.gameObject.layer == 0) {
+                   //modifiers.range = (hit.point - temp.transform.position).magnitude / 250;
+                    return;
+                }
+            } 
+            spellCollision.setModifiers(modifiers);
+        }
+    }
+
+    public override void fire(SpellMod modifiers, Transform firePoint, Transform target) {
+        //Implement for tracking?
+        return;
+    }
+
+    public override GameObject getProjectile() {
+        return projectile;
+    }
+    public override SpellAttr GetSpellAttr() {
+        return spellAttributes;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() {
+
+    }
+}
